dad201c378a6a7a99c63a24f9870acbd87e3368f
Hans García <82074617+HansGV1@users.noreply.github.com>
2022-11-29 16:03:17 -0500

    Update

--- pregunta.py
+++ pregunta.py
@@ -10,11 +10,21 @@ import pandas as pd
 
 
 def clean_data():
+    # Lectura de datos MyFrame y orden
+    MyFrame = pd.read_csv("solicitudes_credito.csv", sep=";")
+    MyFrame.dropna(axis = 0, inplace = True)
+    MyFrame.drop_duplicates(inplace = True)
+    # Fechas
+    MyFrame.fecha_de_beneficio = pd.to_datetime(MyFrame.fecha_de_beneficio, infer_datetime_format = True, errors = "ignore", dayfirst = True)
+    MyFrame.fecha_de_beneficio = MyFrame.fecha_de_beneficio.dt.strftime("%Y/%m/%d")
+    MyFrame = MyFrame.drop(['Unnamed: 0'], axis=1)
+    # Arreglo headers & replace
+    MyFrame[["sexo", "tipo_de_emprendimiento", "idea_negocio", "barrio", "línea_credito"]] = MyFrame[["sexo", "tipo_de_emprendimiento", "idea_negocio", "barrio", "línea_credito"]].apply(lambda x: x.astype(str).str.lower())
+    MyFrame = MyFrame.replace(to_replace="(_)|(-)", value=" ", regex=True)    
+    MyFrame = MyFrame.replace(to_replace="[,$]|(\.00$)", value="", regex=True)
+    # Cast de tipo de datos necesarios 
+    MyFrame.monto_del_credito = MyFrame.monto_del_credito.astype("int")
+    MyFrame.comuna_ciudadano = MyFrame.comuna_ciudadano.astype("float")
+    MyFrame.drop_duplicates(inplace = True)
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
-    return df
+    return MyFrame
